febddc0850cc83a8fc3ceec08d2b24b0032d628d
Arjios <antonio.uab.ios.fisica@gmail.com>
2024-11-29 16:01:04 -0300

    User edition

--- frontend-app-vagacerta/src/screens/Login/index.tsx
+++ frontend-app-vagacerta/src/screens/Login/index.tsx
@@ -1,6 +1,6 @@
 import { Image } from 'react-native';
-import {useState} from 'react'
-import { Wrapper,Container, Form, TextContainer, TextBlack, TextLink, TextLinkContainer } from './styles';
+import { useState } from 'react'
+import { Wrapper, Container, Form, TextContainer, TextBlack, TextLink, TextLinkContainer } from './styles';
 import api from '../../services/api';
 
 import BGTop from '../../assets/BGTop.png';
@@ -12,61 +12,66 @@ import { Button } from '../../components/Button';
 export default function Login({ navigation }) {
 
 
-    const [email, setEmail] = useState('');
-    const [senha, setSenha] = useState('');
+  const [email, setEmail] = useState('');
+  const [senha, setSenha] = useState('');
 
 
-    const handleLogin = async () => {
-        try {
-          const response = await api.get('api/usuarios');
-          const users = response.data.usuarios;
-          console.log(users);
-    
-          const user = users.find(u => u.email === email && u.senha === senha);
-    
-          if (user) {
-            console.log('Login successful', `Welcome, ${user.nome}!`);
-            navigation.navigate('Auth', { screen: 'Home' });
-            // Navegue para a próxima tela ou faça outras ações necessárias
-            // navigation.navigate('NextScreen');
-          } else {
-            console.log('Login failed', 'Email or password is incorrect');
-          }
-        } catch (error) {
-          console.error(error);
-          console.log('Login failed', 'An error occurred during login');
-        }
-      };
+  const handleLogin = async () => {
+    try {
+      const response = await api.get('api/usuarios');
+      const users = response.data.usuarios;
 
-    return (
-        <Wrapper>
-            <Image source={BGTop} />
+      const user = users.find(u => u.email === email && u.senha === senha);
 
-            <Container>
+      if (user) {
+        console.log('Login successful', `Welcome, ${user.nome}!`);
+        navigation.navigate('Auth', { screen: 'Home' });
+        // Navegue para a próxima tela ou faça outras ações necessárias
+        // navigation.navigate('NextScreen');
+      } else {
+        console.log('Login failed', 'Email or password is incorrect');
+      }
+    } catch (error) {
+      console.error(error);
+      console.log('Login failed', 'An error occurred during login');
+    }
+  };
 
-                <Form>
-                    <Logo />
-                    <Input label='E-mail' placeholder='digite seu e-mail' value={email}
-        onChangeText={setEmail}/>
-                    <Input label='Senha' placeholder='digite sua senha' value={senha}
-        onChangeText={setSenha}/>
-                    <Button 
-                    title="Entrar" 
-                    noSpacing={true} 
-                    variant='primary'
-                    onPress={handleLogin}
-                    />
-                    <TextContainer>
-                        <TextBlack>Não tem uma conta?</TextBlack>
-                        <TextLinkContainer onPress={() => navigation.navigate('FormScreen')}>
-                            <TextLink>
-                                    Crie agora mesmo.
-                            </TextLink>
-                        </TextLinkContainer>
-                    </TextContainer>
-                </Form>
+  return (
+    <Wrapper>
+      <Image source={BGTop} />
 
-            </Container>
-        </Wrapper>
-    );
+      <Container>
+
+        <Form>
+          <Logo />
+          <Input
+            label='E-mail'
+            placeholder='digite seu e-mail'
+            value={email}
+            onChangeText={setEmail} />
+          <Input
+            label='Senha'
+            placeholder='digite sua senha'
+            value={senha}
+            onChangeText={setSenha} />
+          <Button
+            title="Entrar"
+            noSpacing={true}
+            variant='primary'
+            onPress={handleLogin}
+          />
+          <TextContainer>
+            <TextBlack>Não tem uma conta?</TextBlack>
+            <TextLinkContainer onPress={() => navigation.navigate('FormScreen')}>
+              <TextLink>
+                Crie agora mesmo.
+              </TextLink>
+            </TextLinkContainer>
+          </TextContainer>
+        </Form>
+
+      </Container>
+    </Wrapper>
+  );
 }
--- frontend-app-vagacerta/src/screens/Profile/index.tsx
+++ frontend-app-vagacerta/src/screens/Profile/index.tsx
@@ -1,11 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Feather } from '@expo/vector-icons';
-import { 
+import {
     Wrapper,
-    Container, 
-    Header, 
-    HeaderButtonContainer, 
-    ButtonIcon, 
+    Container,
+    Header,
+    HeaderButtonContainer,
+    ButtonIcon,
     ButtonText,
     ContentContainer,
 } from '../Profile/styles';
@@ -14,8 +14,25 @@ import theme from '../../theme';
 import Input from '../../components/Input'
 import { Button } from '../../components/Button';
 
+import api from '../../services/api'
+import userRepository from '../../../../backend-app-vagacerta/repositories/usuarioRepository'
 
-export default function Profile({navigation }) {
+
+export default function Profile({ navigation }) {
+    const [nome, setNome] = useState('')
+    const [email, setEmail] = useState('')
+    const [senha, setSenha] = useState('')
+
+    const handleProfile = async () => {
+        const response = await api.get('api/usuarios/: id')
+        const user = response.data
+        if(user) {
+            setNome(user.nome)
+            setEmail(user.email)
+            setSenha(user.senha)
+            userRepository.update(user.id, {nome, email, senha})
+        }
+    }
 
     return (
         <Wrapper>
@@ -33,16 +50,29 @@ export default function Profile({navigation }) {
 
             <Container>
                 <ContentContainer>
-                    <Input label='Nome' placeholder='digite seu nome'/>
-                    <Input label='E-mail' placeholder='digite seu e-mail'/>
-                    <Input label='Senha' placeholder='digite sua senha'/>
+                    <Input label='Nome'
+                        placeholder='digite seu nome'
+                        value={nome}
+                        onChangeText={setNome}
+                    />
+                    <Input label='E-mail'
+                        placeholder='digite seu e-mail' 
+                        value={email}
+                        onChangeText={setEmail}    
+                    />
+                    <Input label='Senha'
+                        placeholder='digite sua senha' 
+                        value={senha}
+                        onChangeText={setSenha}
+                    />
                 </ContentContainer>
 
-                <Button 
-                    title="Salvar informações" 
-                    noSpacing={true} 
+                <Button
+                    title="Salvar informações"
+                    noSpacing={true}
                     variant='primary'
-                    />
+                    onPress={handleProfile}
+                />
             </Container>
         </Wrapper>
     );
